4da4278ee4ca682e43fba29b361e8fd5089a68f8
guhenrico <guga.lucisan@gmail.com>
2024-06-13 00:08:17 -0300

    Criado a página de cadastro, teste de conexão com o banco e o paciente

---
+++ Backend/cadastro.php
@@ -0,0 +1,8 @@
+<?php
+    $ID = $_POST ['ID'];
+    $Nome = $_POST ['Nome'];
+    $strcon = mysqli_connect ("localhost","root","","cadastro") or die ("Erro ao conectar com o banco");
+    $sql = "INSERT INTO cadastro VALUES ('".$ID."' , '".$Nome."');"; 
+    mysqli_query($strcon, $sql) or die ('Erro ao tentar cadastrar registro'); 
+    echo "Paciente Cadastrado com sucesso !";
+?>
---
+++ Backend/paciente.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Paciente</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+    <body>
+        <h3> Cadastro de Pacientes</h3><br>
+        <form name = "Cadastro" action = "cadastro.php" method = "POST">
+        <label> ID: </label>
+        <input type = "text" name = "ID" size = "50" ></br>
+        <label> Nome : </label>
+        <input type = "text" name = "Nome" size = "46" ></br>
+
+        <input type = "submit" name = "enviar" value = "Cadastrar">
+        </form>
+
+    </body>
+</html>
